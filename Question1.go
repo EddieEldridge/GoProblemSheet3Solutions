1bde4f648ac13bbf2b8cab71ba6c547e40192932
EDWARD ELDRIDGE - Student <G00337490@gmit.ie>
2017-11-15 09:22:40 +0000

    More code written

--- Question1.go
+++ Question1.go
@@ -1,1 +1,21 @@
-asdasdas
+// Regular Expressions
+// Edward Eldridge (G00337490)
+
+package main
+
+import fmt
+import bufio
+
+// Function that takes a single string as an output
+func ElizaResponse() string
+{
+	// Variables
+	var userInput string; 
+
+	// Take in user input
+	// Create new reader, assuming bufio imported
+	reader := bufio.NewReader(os.Stdin) 
+	userInput, _ := reader.ReadString('\n')
+
+
+}
